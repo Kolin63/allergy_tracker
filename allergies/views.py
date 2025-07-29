61fed0f64daaf9f5ccaddd52c816bbed00651214
Ayan Desai <ayan.k.desai@gmail.com>
2025-07-29 18:37:39 -0400

    fixed many, many bugs with the allergy CRUD

--- allergies/views.py
+++ allergies/views.py
@@ -1,41 +1,51 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import get_user_model
 from .models import Allergy
 
+CustomUser= get_user_model()
+
 # Create your views here.
-def allergies(request):
-   myallergies = Allergy.objects.all().values()
-   template = loader.get_template('all_allergies.html')
-   context = {
-      'myallergies':myallergies,
-   }
+def allergies(request, user_id):
+   user = get_object_or_404(CustomUser, id=user_id)
+   queryset = user.allergies.all()
+
    if request.method == 'POST':
       data = request.POST
       allergyname = data.get('allergyname')
       test_level = data.get('test_level')
       category = data.get('category')
 
-      Allergy.objects.create(
-         allergyname=allergyname,
-         test_level = test_level,
-         category = category,
+      new_allergy = Allergy.objects.create(
+      allergyname=allergyname,
+      test_level=test_level,
+      category=category,
+    )
+      user.allergies.add(new_allergy)
 
-      )
-      return redirect('/')
-   queryset = Allergy.objects.all()
+      return redirect('user_details', user_id=user.id)
+
+
    if request.GET.get('search'):
       queryset = queryset.filter(allergyname__icontains=request.GET.get('search'))
    
-   context = {'allergies' : queryset}
-   return HttpResponse(template.render(context, request))
+   context = {
+      'myallergies':queryset, 'myuser':user
+   }   
+   return render(request, 'user_details.html', context)
 
 def delete_allergy(request, id):
    allergy = get_object_or_404(Allergy, id=id)
-   allergy.delete()
-   return redirect ('/')
+   
+   user.allergies.remove(allergy)
+   return redirect('user_details', user_id=user.id)
 
 def update_allergy(request, id):
+   allergy = get_object_or_404(Allergy, id=id)
+
+   user = allergy.customuser_set.first() 
+
    if request.method == 'POST':
       data = request.POST
       allergyname = data.get('allergyname')
@@ -47,14 +57,15 @@ def update_allergy(request, id):
       allergy.category = category
       allergy.save()
       context = {'allergy': allergy}
-      return render (request 'details.html', context)
+      return redirect('user_details', user_id=user.id)
+   
+   allergies = user.allergies.all()
+   return render(request, 'user_details.html', {'myuser': user, 'allergies': allergies, 'selected_allergy': allergy})
+
+
 def details(request, id):
-   myallergy = Allergy.objects.get(id=id)
-   template = loader.get_template('details.html')
-   context = {
-      'myallergy': myallergy
-   }
-   return HttpResponse(template.render(context,request))
-def main(request):
-   template = loader.get_template('main.html')
-   return HttpResponse(template.render())
+   user = get_object_or_404(CustomUser, id=id)
+   allergies = user.allergies.all()
+   return render(request, 'user_details.html', {'myuser': user, 'allergies': allergies})
+
+
