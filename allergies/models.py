8fbd1b4736fc98d4aa89ab411084e2cbfe6e9022
Ayan Desai <ayan.k.desai@gmail.com>
2025-08-20 19:05:20 -0400

    added logic and allergy badges

--- allergies/models.py
+++ allergies/models.py
@@ -20,5 +20,22 @@ class Allergy(models.Model):
     test_level = models.DecimalField(null=True, max_digits=10, decimal_places=2,)
     category = models.CharField(max_length=255, null=True) 
 
+    def severity(self):
+        if self.test_level < 3.0:
+            return "Mild"
+        elif self.test_level < 6.0:
+            return "Moderate"
+        return "Severe"
+
+    def severity_badge_class(self):
+        if self.test_level < 3.0:
+            return "bg-success"  # green
+        elif self.test_level < 6.0:
+            return "bg-warning text-dark"  # orange
+        return "bg-danger"  # red
+
+
     def __str__(self):
         return self.allergyname
+    
+
